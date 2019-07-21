855dcef89f9ead42b361e75209eda87114d71251
rajat <rajatthakur0026@gmail.com>
2019-07-21 12:02:09 +0530

    added Pet Type with Bootstrap startup, Closes #32

--- Pet-Clinic-Web/src/main/java/com/springframework/petcare/bootstrap/DataLoader.java
+++ Pet-Clinic-Web/src/main/java/com/springframework/petcare/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.springframework.petcare.bootstrap;
 
 import com.springframework.petcare.model.Owner;
+import com.springframework.petcare.model.PetType;
 import com.springframework.petcare.model.Vet;
 import com.springframework.petcare.services.OwnerService;
+import com.springframework.petcare.services.PetTypeService;
 import com.springframework.petcare.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -15,15 +17,30 @@ public class DataLoader implements CommandLineRunner {
     private static final Logger logger = Logger.getLogger(DataLoader.class.getName());
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
+
     }
 
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType saveDogPetType = petTypeService.save(dog); //later to use with owner and vets.
+
+
+        PetType cat = new PetType();
+        dog.setName("Cat");
+        PetType saveCatPetType = petTypeService.save(cat); //later to use with owner and vets.
+
+        logger.info("Loading PetType");
+
         Owner owner = new Owner();
         owner.setFirstName("Rajat");
         owner.setLastName("Thakur");
@@ -36,7 +53,7 @@ public class DataLoader implements CommandLineRunner {
 
         ownerService.save(owner2);
 
-        logger.info("Loaded Owners");
+        logger.info("Loading Owners");
 
         Vet vet = new Vet();
         vet.setFirstName("vetRajat");
@@ -51,7 +68,9 @@ public class DataLoader implements CommandLineRunner {
         vetService.save(vet2);
 
 
-        logger.info("Loaded Vets");
+        logger.info("Loading Vets");
+
+
     }
 }
 
