6aa0596138cc396871823109add6a43f812d1ca5
rajat <rajatthakur0026@gmail.com>
2019-07-21 15:16:39 +0530

    added Specialty Map Service impl , Closes #31

---
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/services/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.springframework.petcare.services;
+
+import com.springframework.petcare.model.Speciality;
+
+public interface SpecialtyService extends CrudService<Speciality,Long> {
+}
---
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/services/map/SpecialtyServiceMap.java
@@ -0,0 +1,35 @@
+package com.springframework.petcare.services.map;
+
+import com.springframework.petcare.model.Speciality;
+import com.springframework.petcare.services.SpecialtyService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class SpecialtyServiceMap extends AbstractMapService<Speciality, Long> implements SpecialtyService {
+    @Override
+    public Set<Speciality> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Speciality object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Speciality save(Speciality object) {
+        return super.save(object);
+    }
+
+    @Override
+    public Speciality findById(Long id) {
+        return super.findById(id);
+    }
+}
