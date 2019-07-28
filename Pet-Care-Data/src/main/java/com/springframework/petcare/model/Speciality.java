b6769667447cee70187f3cba3b1d0260d79df9f1
rajat <rajatthakur0026@gmail.com>
2019-07-28 17:47:04 +0530

    updated  Vets to JPA Entities, Closes #37

--- Pet-Care-Data/src/main/java/com/springframework/petcare/model/Speciality.java
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/model/Speciality.java
@@ -1,6 +1,14 @@
 package com.springframework.petcare.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "specialities")
 public class Speciality extends BaseEntity {
+
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
