88d90390ecf02a0e491b4cf56d749354cfa12d22
rajat <rajatthakur0026@gmail.com>
2019-07-28 17:26:36 +0530

    updated Owner with JPA Entities, Closes #36

--- Pet-Care-Data/src/main/java/com/springframework/petcare/model/Owner.java
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/model/Owner.java
@@ -1,12 +1,23 @@
 package com.springframework.petcare.model;
 
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Entity
+@Table(name = "owners")
 public class Owner extends Person {
+
+    @Column(name = "address")
     private String address;
+
+    @Column(name = "city")
     private String city;
+
+    @Column(name = "telephone")
     private String telephone;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "owner")//this Owner will get stored on a property of a child
     private Set<Pet> pets = new HashSet<>();
 
     public String getAddress() {
