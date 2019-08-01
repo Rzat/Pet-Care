22f6607bf0c2aa4546a9d5dd665497365ac1291d
Rzat <rajatthakur00@gmail.com>
2019-08-01 12:36:59 +0530

    Implemented Visit Map Service, Closes #46

---
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/services/VisitService.java
@@ -0,0 +1,6 @@
+package com.springframework.petcare.services;
+
+import com.springframework.petcare.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
---
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/services/map/VisitServiceMap.java
@@ -0,0 +1,38 @@
+package com.springframework.petcare.services.map;
+
+import com.springframework.petcare.model.Visit;
+import com.springframework.petcare.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+public class VisitServiceMap extends AbstractMapService<Visit, Long> implements VisitService {
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if (visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null) {
+            throw new RuntimeException("Invalid Visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return super.findById(id);
+    }
+}
