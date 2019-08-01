cd2b9a6beff4bc4161059af862ccf84b2d767f29
Rzat <rajatthakur00@gmail.com>
2019-08-01 12:52:31 +0530

    Implemented Spring Data Visit Jpa Service for Map, Closes #47

---
+++ Pet-Care-Data/src/main/java/com/springframework/petcare/services/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,47 @@
+package com.springframework.petcare.services.springdatajpa;
+
+import com.springframework.petcare.model.Visit;
+import com.springframework.petcare.repositories.VisitRepository;
+import com.springframework.petcare.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@Profile("springdatajpa")
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visits = new HashSet<>();
+        visitRepository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElseThrow(() -> new RuntimeException("Can not found visit id: " + aLong));
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+}
