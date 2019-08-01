602846737477c4772d62654066915fdd6c7f0227
Rzat <rajatthakur00@gmail.com>
2019-08-01 17:53:50 +0530

    Adding Visit on Startup in Bootstrap, Closes #48

--- Pet-Clinic-Web/src/main/java/com/springframework/petcare/bootstrap/DataLoader.java
+++ Pet-Clinic-Web/src/main/java/com/springframework/petcare/bootstrap/DataLoader.java
@@ -1,10 +1,7 @@
 package com.springframework.petcare.bootstrap;
 
 import com.springframework.petcare.model.*;
-import com.springframework.petcare.services.OwnerService;
-import com.springframework.petcare.services.PetTypeService;
-import com.springframework.petcare.services.SpecialtyService;
-import com.springframework.petcare.services.VetService;
+import com.springframework.petcare.services.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -21,14 +18,15 @@ public class DataLoader implements CommandLineRunner {
     private final VetService vetService;
     private final PetTypeService petTypeService;
     private final SpecialtyService specialtyService;
-
+    private final VisitService visitService;
 
     public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService,
-                      SpecialtyService specialtyService) {
+                      SpecialtyService specialtyService, VisitService visitService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
         this.petTypeService = petTypeService;
         this.specialtyService = specialtyService;
+        this.visitService = visitService;
     }
 
 
@@ -85,6 +83,14 @@ public class DataLoader implements CommandLineRunner {
         owner2.getPets().add(yoursPet);
 
         ownerService.save(owner2);
+
+        Visit catVisit = new Visit();
+        catVisit.setPet(yoursPet);
+        catVisit.setDescription("Snezzy Kiti");
+        catVisit.setLocalDate(LocalDate.now());
+        visitService.save(catVisit);
+
+        logger.info("Loading Visit");
 
         logger.info("Loading Owners");
 
